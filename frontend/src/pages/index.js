e554e3030eac0b2a02f130090e863d467efb1c19
cathyai0320 <cathystudyingai@gmail.com>
2024-04-07 17:54:46 -0400

    frontend UI

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
- 
+
 const Home = () => {
   return (
     <div className="main-photo">
       <div className="overlay-content">
         <h1 className="overlay-title">CropSense: The Intelligence Crop Recommender</h1>
-        <p>CropSense addresses pressing agricultural challenges by introducing an innovative Generative AI-based solution to enhance crop yields and soil health amid changing environmental conditions and demographic shifts in the farming sector.</p>
+        <p>
+          CropSense is at the forefront of agricultural innovation. 
+          Our Generative AI-based solution is designed to revolutionize crop management.
+        </p>
+        <p>
+          By enhancing crop yields and improving soil health, we aim to navigate 
+          the changing environmental landscape and adapt to demographic shifts in farming.
+        </p>
       </div>
     </div>
   );
